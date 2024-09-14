c2ad174c540ef4139f51d618f652a4c6114947dd
gary-rivera <a.gary.rivera@gmail.com>
2024-09-13 17:20:56 -0700

    dummy parent, child, and grandchild nodes

---
+++ src/components/dummy/ParentComponent.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ChildComponent from './ChildComponent'
+
+const ParentComponent: React.FC = () => {
+  return (
+    <div style={{ padding: '20px', background: 'gray' }}>
+      <h1>Parent Component</h1>
+      <p>This is the parent component.</p>
+      <ChildComponent />
+      <button>Click Me</button>
+    </div>
+  )
+}
+
+export default ParentComponent
