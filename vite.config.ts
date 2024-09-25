145ec266a6b6c393f9d8b7d28ca45f61c55c4a98
gary-rivera <a.gary.rivera@gmail.com>
2024-09-24 22:52:14 -0700

    reconfigure tsc (still testing)

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 import svgr from 'vite-plugin-svgr'
+import checker from 'vite-plugin-checker'
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), checker({ typescript: true })],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/components/RuioWrapper.tsx'),
+      name: 'Ruio',
+      fileName: (format) => `ruio.${format}.js`
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM'
+        }
+      }
+    }
+  },
   resolve: {
     alias: {
       '@assets': path.resolve(__dirname, './src/assets'),
@@ -15,6 +32,11 @@ export default defineConfig({
       '@context': path.resolve(__dirname, './src/context'),
     },
   },
+  server: {
+    hmr: {
+      overlay: true
+    }
+  }
 })
 
 // TODO: leverage HMR https://stackoverflow.com/questions/70996320/enable-hot-reload-for-vite-react-project-instead-of-page-reload
