14e3ed0ef7020d70c7961159f56581667eeae98b
gary-rivera <a.gary.rivera@gmail.com>
2024-10-04 17:03:13 -0700

    define CloseModalIcon + styling

---
+++ src/components/icons/CloseModalIcon.tsx
@@ -0,0 +1,36 @@
+import { useRef } from 'react'
+import { useRuioContext } from '@root/context/RuioContextProvider'
+import RuioIcon from '@components/icons/RuioIcon'
+import IconProps from '../../types/IconTypes'
+
+import buttonStyles from '../../styles/Button.module.css'
+import iconStyles from '../../styles/Icon.module.css'
+import svgStyles from '../../styles/SVG.module.css'
+
+function CloseModalIcon({ onClick }: IconProps) {
+  const iconRef = useRef<HTMLDivElement>(null)
+
+  return (
+    <RuioIcon
+      onClick={onClick}
+      buttonClassName={`
+          ruio-exclude
+          ${buttonStyles['ruio-btn']}
+          ${iconStyles['close-modal-btn']}
+        `}
+      svgClassName={` ${svgStyles['close-modal-svg']}`}
+      svgViewBox="0 0 94 93"
+    >
+      <>
+        <path
+          d="M11.9982 11.2911L82.7089 82.0018M11.2911 82.0018L82.0018 11.2911"
+          stroke="white"
+          stroke-width="22"
+          stroke-linecap="round"
+        />
+      </>
+    </RuioIcon>
+  )
+}
+
+export default CloseModalIcon
