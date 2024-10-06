dbaebb26228bea734da6499a253112ce9ad59865
gary-rivera <a.gary.rivera@gmail.com>
2024-10-05 17:02:46 -0700

    apply chevron styles and animations

--- src/components/icons/ChevronIcon.tsx
+++ src/components/icons/ChevronIcon.tsx
@@ -7,28 +7,23 @@ import buttonStyles from '../../styles/Button.module.css'
 import iconStyles from '../../styles/Icon.module.css'
 import svgStyles from '../../styles/SVG.module.css'
 
-function ChevronIcon() {
+type ChevronIconProps = {
+  isOpen?: boolean
+}
+
+function ChevronIcon({ isOpen }: ChevronIconProps) {
   return (
     <RuioIcon
       id="ruio-chevron"
       buttonClassName={`
           ruio-exclude
           ${buttonStyles['ruio-btn']}
-          ${iconStyles['ruio-chevron']}
+          ${iconStyles.chevron}
+          ${isOpen ? iconStyles.chevronActive : iconStyles.chevronInactive}
         `}
-      svgClassName={` ${svgStyles['close-modal-svg']}`}
+      svgClassName={` ${svgStyles['ruio-chevron-svg']}`}
       svgViewBox="0 0 92 62"
     >
-      <>
-        {/* <svg width="92" height="62" viewBox="0 0 92 62" fill="none" xmlns="http://www.w3.org/2000/svg"> */}
-        <path
-          d="M10.9982 11.2911L46 46.2929L81.0018 11.2911"
-          stroke="white"
-          stroke-width="22"
-          stroke-linecap="round"
-        />
-        {/* </svg> */}
-      </>
     </RuioIcon>
   )
 }
