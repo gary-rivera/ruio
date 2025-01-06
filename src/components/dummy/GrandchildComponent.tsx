51cd7b6ad0d00950a5a38d3409bdbe986f34707a
gary-rivera <a.gary.rivera@gmail.com>
2025-01-06 10:37:58 -0800

    cleanup/prep/configure

--- src/components/dummy/GrandchildComponent.tsx
+++ src/components/dummy/GrandchildComponent.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const GrandChildComponent: React.FC = () => {
   return (
-    <div style={{ padding: '20px', background: 'lightgray', border: '1px solid black' }}>
+    <div
+      style={{ padding: '20px', background: 'lightgray', border: '1px solid black' }}
+      id="ruio-grandchild-component"
+    >
       <h3>Grandchild Component</h3>
       <p>This is the grandchild component, nested inside the child component.</p>
     </div>
