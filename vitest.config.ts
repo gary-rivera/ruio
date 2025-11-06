a24c5d74e373d822342e060f184bfda4049ede6c
gary-rivera <a.gary.rivera@gmail.com>
2025-11-05 16:22:59 -0800

    install and configure vitest + uninstall jest
    (tests failing)

---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./test/vitest.setup.ts'],
+    include: ['test/**/*.test.{ts,tsx}'],
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'test/',
+        'dist/',
+        '**/*.d.ts',
+        '**/*.config.*',
+        '**/mockData',
+        '**/__mocks__',
+      ],
+    },
+  },
+  resolve: {
+    alias: {
+      '@components': resolve(__dirname, './src/components'),
+      '@context': resolve(__dirname, './src/context'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@constants': resolve(__dirname, './src/constants'),
+      '@controllers': resolve(__dirname, './src/controllers'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@root': resolve(__dirname, './src'),
+      '@assets': resolve(__dirname, './src/assets'),
+    },
+  },
+})
